bcd5958d4320a14ab1a747f70ca34e5c3749528e
Ayibadeinyefa <dein.warde@gmail.com>
2025-02-27 23:02:14 +0100

    Created Player ViewSet with ModelViewSet

--- players/views.py
+++ players/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Player
+from .serialers import PlayerSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class PlayerViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
