b4425803c34bf84b05529c2ec75ba1cc0e5c7bb1
Ayibadeinyefa <dein.warde@gmail.com>
2025-02-28 21:47:00 +0100

    Added login and signup URLs

--- players/urls.py
+++ players/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PlayerViewSet
+from .views import PlayerViewSet, SignupView, LoginView
 
 # Create a router and register the PlayerViewSet
 router = DefaultRouter()
 router.register(r'players', PlayerViewSet)  # This maps /players/ to PlayerViewSet
 
 urlpatterns = [
-    path('', include(router.urls)),  # Include all router-generated URLs
+    path('', include(router.urls)),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),  
 ]
