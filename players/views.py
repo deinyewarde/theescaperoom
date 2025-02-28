43a946a97a1c268058ef7d2685a6c43f5b9b2fb7
Ayibadeinyefa <dein.warde@gmail.com>
2025-02-28 21:43:10 +0100

    Added signup and login views

--- players/views.py
+++ players/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 from .models import Player
-from .serializers import PlayerSerializer
+from .serializers import PlayerSerializer, SignupSerializer
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
 
+class SignupView(APIView):
+    def post(self, request):
+        serializer = SignupSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)  # Generate token
+            return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        
+        if user:
+            token, created = Token.objects.get_or_create(user=user)  # Get or create token
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
